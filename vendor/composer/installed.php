864b59311bd83ed937588dce3c508892d181f335
Erick Cantillo <erick.cantillo97@gmail.com>
2025-10-07 07:21:17 -0500

    Actualizar .gitignore para incluir config/config.php; ajustar márgenes en global.css y agregar estilos responsivos; corregir referencias en installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '8ad525f141bdbe335498c48bdbc67dd3f806072f',
+        'reference' => '81585ee8eb41fc579e95022b943dbb673c93b94c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '8ad525f141bdbe335498c48bdbc67dd3f806072f',
+            'reference' => '81585ee8eb41fc579e95022b943dbb673c93b94c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
