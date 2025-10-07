358e215e5bc65f6ebd2e4b6b95c66aeb53850025
Erick Cantillo <erick.cantillo97@gmail.com>
2025-10-07 16:26:19 -0500

    Corregir clase de entrada de contraseña en el formulario de inicio de sesión y actualizar la referencia en el archivo instalado.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '81585ee8eb41fc579e95022b943dbb673c93b94c',
+        'reference' => '39dd31feb064553634117f04055a4896c0a2dca2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '81585ee8eb41fc579e95022b943dbb673c93b94c',
+            'reference' => '39dd31feb064553634117f04055a4896c0a2dca2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
